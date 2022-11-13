c281c55ff9613296b92e58b35fd5801bdbe8b3d3
Michael Uyanna <crackmastermike@gmail.com>
2022-11-13 14:22:52 +0100

    created hero and header components

--- src/assets/components/header/header.component.js
+++ src/assets/components/header/header.component.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from "../../images/logo-header.png"
+
 const Header = () => {
-    return <div>This is the header</div>
+    return (
+        <header>
+            <div className="logo"><img src={logo} alt="logo-header" /></div>
+            <nav>
+                <Link to="#">Home</Link>
+                <Link to="#">Place to stay</Link>
+                <Link to="#">NFTs</Link>
+                <Link to="#">Community</Link>
+            </nav>
+            <button>Connect Wallet</button>
+        </header>
+    )
 }
 
 export default Header;
---
+++ src/assets/components/hero/hero.component.js
@@ -0,0 +1,21 @@
+import heroimg from "../../images/hero-img.png"
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-info">
+                <h1>Rent a <span className="keyword">Place</span> away from
+                    <span className="keyword">Home</span> in the <span className="keyword">Metaverse</span></h1>
+                <p>we provide you access to luxury and affordable houses in the metaverse,
+                    get a chance to turn your imagination to reality at your comfort zone</p>
+                <form>
+                    <input type="search" name="search" placeholder="Search for location" />
+                    <button className="btn" type="submit">Search</button>
+                </form>
+            </div>
+            <div className="hero-img"><img src={heroimg} alt="hero-img" /></div>
+        </div>
+    )
+}
+
+export default Hero
