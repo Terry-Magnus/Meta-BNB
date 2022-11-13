b4033298953e6bbc8683e1dcd792983a7a2e303a
Michael Uyanna <crackmastermike@gmail.com>
2022-11-13 07:59:42 +0100

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./assets/components/header/header.component"
+import Footer from "./assets/components/footer/footer.component";
 import './App.css';
+
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
         </div>
     );
 }
