21dfaf6ec7dda1360e99a04d178a7696a2850908
Michael Uyanna <crackmastermike@gmail.com>
2022-11-14 09:12:52 +0100

    Updated places route

--- src/assets/pages/places.js
+++ src/assets/pages/places.js
@@ -1,7 +1,25 @@
 import Header from "../components/header.component";
+import filter from "../images/filters.png"
 
 const Places = () => {
-    return <Header />
+    return (
+        <>
+            <Header />
+            <div className="filters">
+                <ul>
+                    <li>Restaurant</li>
+                    <li>Cottage</li>
+                    <li>Castle</li>
+                    <li>Fantasy City</li>
+                    <li>Beach</li>
+                    <li>Cabin</li>
+                    <li>Off-grid</li>
+                    <li>Farm</li>
+                </ul>
+                <button className="filter">Location <img src={filter} alt="filter" /></button>
+            </div>
+        </>
+    )
 }
 
 export default Places;
