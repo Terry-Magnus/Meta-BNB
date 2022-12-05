075d474c97c6483c631032b47dfc440f64d1a977
Michael Uyanna <crackmastermike@gmail.com>
2022-12-05 09:18:14 +0100

    added hashrouter for react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Places from "./assets/pages/places"
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App />,
